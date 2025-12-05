c8dd909e8a754b5c9b231b6f27a27d67b1829ff8
mysalebyms-sketch <mysalebyms@gmail.com>
2025-12-05 17:18:12 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+// config.js - ใส่ URL ของ Apps Script Web App ที่คุณ Deploy
+
+// ❗ แก้ค่านี้ให้เป็นเว็บแอปของคุณเอง
+typeof APPS_SCRIPT_URL === 'undefined';
+const APPS_SCRIPT_URL = 'PASTE_YOUR_WEB_APP_URL_HERE';
+
+// ตัวอย่าง:
+// const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxxxxxx/exec';
