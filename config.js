38e6ba9ddc8dbcdc6df698858b41366bf04e3bbb
mysalebyms-sketch <mysalebyms@gmail.com>
2025-12-05 17:51:05 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,7 @@
 // config.js - ใส่ URL ของ Apps Script Web App ที่คุณ Deploy
 
-// ❗ แก้ค่านี้ให้เป็นเว็บแอปของคุณเอง
-typeof APPS_SCRIPT_URL === 'undefined';
-const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbymrw5IvZQknZEqjm57AOJ68sbvZaBMsnLBflvXZsl51pgVgTK98Mjt1crqgH6RehScug/exec';
+// ❗ ใส่ URL เว็บแอปของคุณเอง
+const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbymrw5IvZQknZEqjm57AOJ68sbvZaBMsnLBflvXZsl51pgVgTK98Mjt1crqgH6RehScug/exec";
 
-// ตัวอย่าง:
-// const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxxxxxx/exec';
+// Debug (เลือกใส่หรือไม่ก็ได้)
+console.log("Loaded config.js, APPS_SCRIPT_URL =", APPS_SCRIPT_URL);
