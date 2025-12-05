f20492e1722c27b9a2d5d70e1b5f07578d4b9dab
mysalebyms-sketch <mysalebyms@gmail.com>
2025-12-05 17:28:43 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 
 // ❗ แก้ค่านี้ให้เป็นเว็บแอปของคุณเอง
 typeof APPS_SCRIPT_URL === 'undefined';
-const APPS_SCRIPT_URL = 'PASTE_YOUR_WEB_APP_URL_HERE';
+const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbymrw5IvZQknZEqjm57AOJ68sbvZaBMsnLBflvXZsl51pgVgTK98Mjt1crqgH6RehScug/exec';
 
 // ตัวอย่าง:
 // const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxxxxxx/exec';
